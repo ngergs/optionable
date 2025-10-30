43fbbe66b27d18c0a9c821452e51d064c1b6ef98
ngergs <niklas@ngergs.de>
2025-10-30 20:37:49 +0100

    chore: rebase

--- optionable/src/k8s_openapi/codegen.sh
+++ optionable/src/k8s_openapi/codegen.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cargo run --manifest-path=../../../optionable_codegen/bin/Cargo.toml -- --replace-crate-name k8s_openapi ../../../../k8s-openapi/src/v1_34/mod.rs v1_34
+cargo run --manifest-path=../../../optionable_codegen/bin/Cargo.toml --bin cli -- --replace-crate-name k8s_openapi ../../../../k8s-openapi/src/v1_34/mod.rs v1_34
 
 
--- optionable_codegen/bin/src/lib.rs
+++ optionable_codegen/bin/src/lib.rs
@@ -1,14 +1,13 @@
 extern crate core;
 
-use clap::Parser;
 use darling::FromMeta;
-use optionable_codegen::{attribute_derives, attribute_no_convert, CodegenSettings};
+use optionable_codegen::CodegenSettings;
 use proc_macro2::{Ident, Span};
 use quote::ToTokens;
 use std::borrow::Cow;
 use std::fs::create_dir_all;
 use std::mem::take;
-use std::path::{Path, PathBuf};
+use std::path::Path;
 use std::{fs, io};
 use syn::Item::{Enum, Mod, Struct, Use};
 use syn::{
@@ -16,76 +15,9 @@ use syn::{
     Visibility,
 };
 
-/// Generates `Optionable` and `OptionableConvert` implementation for structs/enums in
-/// the referenced `input_file` and all included internal modules recursively.
-#[derive(Parser, Debug)]
-#[command(version, about, long_about = None)]
-struct Args {
-    /// Input file.
-    input_file: PathBuf,
-    /// Output directory
-    output_dir: PathBuf,
-    /// Whether to opt-out of generating `OptionableConvert`-trait implementations.
-    #[arg(long, default_value_t = false)]
-    no_convert: bool,
-    /// Identifiers for which derive statements should be added to the generated structs/enums.
-    #[arg(long, short)]
-    derive: Vec<String>,
-    /// Flag for the purpose of generating code that should be added to the optionable crate itself.
-    /// - Replaces the keyword `crate` in the input with the provided string value.
-    /// - Prepends the provided string crate name and the respective package subpath to the `impl` definitions.
-    /// - Uses in the generated code `crate` instead of `::optionable` to refer to the optionable crate.
-    #[arg(long)]
-    replace_crate_name: Option<String>,
-}
-
-pub(crate) fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let args = Args::parse();
-    let type_attrs = input_type_attrs(&args)?;
-    let codegen_settings = input_codegen_settings(&args)?;
-    create_dir_all(&args.output_dir)?;
-    file_codegen(
-        &args.input_file,
-        &args.output_dir,
-        &type_attrs,
-        &codegen_settings,
-        &[],
-        false,
-    )
-}
-
-/// Parses the input args and generated corresponding type attributes
-fn input_type_attrs(args: &Args) -> Result<Vec<Attribute>, Error> {
-    let mut type_attrs = Vec::new();
-    if args.no_convert {
-        type_attrs.push(attribute_no_convert());
-    }
-    if !args.derive.is_empty() {
-        let derives = args
-            .derive
-            .iter()
-            .map(|d| syn::parse_str(d))
-            .collect::<Result<Vec<_>, _>>()?
-            .into();
-
-        type_attrs.push(attribute_derives(&derives));
-    }
-    Ok(type_attrs)
-}
-
-fn input_codegen_settings(args: &Args) -> Result<CodegenSettings, Error> {
-    let mut settings = CodegenSettings::default();
-    if let Some(replace_crate_name) = &args.replace_crate_name {
-        settings.optionable_crate_name = syn::Path::from_string("crate")?;
-        settings.ty_prefix = Some(syn::Path::from_string(&format!("::{replace_crate_name}"))?);
-        settings.input_crate_replacement = Some(Ident::new(replace_crate_name, Span::call_site()));
-    }
-    Ok(settings)
-}
-
 /// Read the respective `input_file` and calls codegen for it. The result is written
 /// into the `output_path` under the same `file_name` as the input.
-fn file_codegen(
+pub fn file_codegen(
     input_file: &Path,
     output_path: &Path,
     type_attrs: &Vec<Attribute>,
