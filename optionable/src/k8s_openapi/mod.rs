c1eac852879f7e1d149d704b394103b4d07c2f0c
ngergs <niklas@ngergs.de>
2025-10-30 23:13:48 +0100

    chore: test

--- optionable/src/k8s_openapi/mod.rs
+++ optionable/src/k8s_openapi/mod.rs
@@ -7,11 +7,14 @@ mod v1_34;
 #[cfg(test)]
 mod test {
     use crate::k8s_openapi::v1_34::api::apps::v1::DeploymentOpt;
+    use k8s_openapi::apimachinery::pkg::apis::meta::v1::ObjectMeta;
 
     #[test]
     fn deployment_opt() {
         let _ = DeploymentOpt {
-            metadata: None,
+            metadata: ObjectMeta {
+                ..Default::default()
+            },
             spec: None,
             status: None,
         };
