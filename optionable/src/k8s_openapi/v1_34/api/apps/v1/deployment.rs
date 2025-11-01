e85c4af3a1a1f11cc6e2e7887f122f52ff224c6a
ngergs <niklas@ngergs.de>
2025-11-01 12:41:55 +0100

    fix: use struct to pass around codegen config (cli)

--- optionable/src/k8s_openapi/v1_34/api/apps/v1/deployment.rs
+++ optionable/src/k8s_openapi/v1_34/api/apps/v1/deployment.rs
@@ -1,10 +1,13 @@
-#[derive(kube::Resource)]
-#[resource(inherit = "k8s_openapi::api::apps::v1::Deployment")]
 pub struct DeploymentOpt {
-    pub metadata: k8s_openapi::apimachinery::pkg::apis::meta::v1::ObjectMeta,
-    pub spec: <Option<::k8s_openapi::api::apps::v1::DeploymentSpec> as crate::Optionable>::Optioned,
-    pub status:
-        <Option<::k8s_openapi::api::apps::v1::DeploymentStatus> as crate::Optionable>::Optioned,
+    pub metadata: Option<
+        <::k8s_openapi::apimachinery::pkg::apis::meta::v1::ObjectMeta as crate::Optionable>::Optioned,
+    >,
+    pub spec: <Option<
+        ::k8s_openapi::api::apps::v1::DeploymentSpec,
+    > as crate::Optionable>::Optioned,
+    pub status: <Option<
+        ::k8s_openapi::api::apps::v1::DeploymentStatus,
+    > as crate::Optionable>::Optioned,
 }
 #[automatically_derived]
 impl crate::Optionable for ::k8s_openapi::api::apps::v1::Deployment {
@@ -13,4 +16,37 @@ impl crate::Optionable for ::k8s_openapi::api::apps::v1::Deployment {
 #[automatically_derived]
 impl crate::Optionable for DeploymentOpt {
     type Optioned = DeploymentOpt;
+}
+#[automatically_derived]
+impl crate::OptionableConvert for ::k8s_openapi::api::apps::v1::Deployment {
+    fn into_optioned(self) -> DeploymentOpt {
+        DeploymentOpt {
+            metadata: Some(crate::OptionableConvert::into_optioned(self.metadata)),
+            spec: crate::OptionableConvert::into_optioned(self.spec),
+            status: crate::OptionableConvert::into_optioned(self.status),
+        }
+    }
+    fn try_from_optioned(
+        value: DeploymentOpt,
+    ) -> Result<Self, crate::optionable::Error> {
+        Ok(Self {
+            metadata: crate::OptionableConvert::try_from_optioned(
+                value
+                    .metadata
+                    .ok_or(crate::optionable::Error {
+                        missing_field: "metadata",
+                    })?,
+            )?,
+            spec: crate::OptionableConvert::try_from_optioned(value.spec)?,
+            status: crate::OptionableConvert::try_from_optioned(value.status)?,
+        })
+    }
+    fn merge(&mut self, other: DeploymentOpt) -> Result<(), crate::optionable::Error> {
+        if let Some(other_value) = other.metadata {
+            crate::OptionableConvert::merge(&mut self.metadata, other_value)?;
+        }
+        crate::OptionableConvert::merge(&mut self.spec, other.spec)?;
+        crate::OptionableConvert::merge(&mut self.status, other.status)?;
+        Ok(())
+    }
 }
