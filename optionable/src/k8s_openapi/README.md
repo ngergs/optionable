80237a5648399cfeccc4c84a5f159b3491f5639f
ngergs <niklas@ngergs.de>
2025-11-06 23:14:02 +0100

    chore: document codegen

---
+++ optionable/src/k8s_openapi/README.md
@@ -0,0 +1,8 @@
+# Codegen
+
+The codegen generates optioned types and implements the `Optionable`-trait for types from [`k8s-openapi`](https://github.com/Arnavion/k8s-openapi).
+
+To regenerate the files run:
+```bash
+./codegen.sh <path to git root of the cloned k8s-openapi repo>
+```
--- optionable/src/k8s_openapi/codegen.sh
+++ optionable/src/k8s_openapi/codegen.sh
@@ -2,5 +2,5 @@
 set -euo pipefail
 for v in {30..34}
 do
-	cargo run --manifest-path=../../../optionable_codegen/bin/Cargo.toml --features k8s_openapi --bin k8s_openapi -- ../../../../k8s-openapi/src/v1_${v}/mod.rs v1_${v}
+	cargo run --manifest-path=../../../optionable_codegen/bin/Cargo.toml --features k8s_openapi --bin k8s_openapi -- $1/src/v1_${v}/mod.rs v1_${v}
 done
