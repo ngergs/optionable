4ca996e49951f11f3543fa10d54ef115336867cd
ngergs <niklas@ngergs.de>
2025-11-14 20:49:40 +0100

    chore: document k8s_openapi version used by codegen

--- optionable/src/k8s_openapi/codegen.sh
+++ optionable/src/k8s_openapi/codegen.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+
 set -euo pipefail
+
+K8S_OPENAPI_VERSION=$(git -C "$1" describe --exact-match --tags)
+echo "The \`v1_*\` folders are generated based on k8s_openapi ${K8S_OPENAPI_VERSION}" > version
+
 for v in {30..34}
 do
-	cargo run --manifest-path=../../../optionable_codegen/bin/Cargo.toml --features k8s_openapi --bin k8s_openapi -- $1/src/v1_${v}/mod.rs v1_${v}
+	cargo run --manifest-path=../../../optionable_codegen/bin/Cargo.toml --features k8s_openapi --bin k8s_openapi -- "$1/src/v1_${v}/mod.rs" "v1_${v}"
 done
