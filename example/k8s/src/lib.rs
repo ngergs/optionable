dea66d354ef8db9e4b4f0bfdcc36f48d89ee800b
ngergs <niklas@ngergs.de>
2025-11-27 22:03:08 +0100

    chore: simplify example derive macro

--- example/k8s/src/lib.rs
+++ example/k8s/src/lib.rs
@@ -1,29 +1,24 @@
+// Example how to use `ApplyConfigurations` derived from `optionable` with a CRD
+
 use kube::CustomResource;
 use optionable::Optionable;
-
-// Example how to use `ApplyConfigurations` derived from `optionable` with a CRD
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
 
 pub const FIELD_MANAGER: &str = "rust-manager";
 
 // Repeating the derives for the spec and subfields as well as for `optionable` can be cumbersome
 // so let's define a simple helper macro for that
 macro_rules! with_common_derives {
-     ($($item:tt)*) => {
-        #[derive(
-            ::optionable::Optionable,
-            Clone,
-            Debug,
-            serde::Deserialize,
-            serde::Serialize,
-            schemars::JsonSchema
-        )]
-        #[optionable(kube(), derive(Clone, Debug, Default, serde::Deserialize, serde::Serialize))]
+    ($item:item) => {
+        #[derive(Optionable, Clone, Debug, Deserialize, Serialize, JsonSchema)]
+        #[optionable(kube(), derive(Clone, Debug, Default, Deserialize, Serialize))]
         #[serde(rename_all = "camelCase")]
-        $($item)*
+        $item
     };
 }
 
-with_common_derives!(
+with_common_derives! {
     #[derive(CustomResource)]
     #[kube(
         group = "example.localhost",
@@ -34,24 +29,24 @@ with_common_derives!(
         derive = "Optionable",
         // Instruct `optionable` to derive the traits and specialized handling for the root type. Detailed docs are here:
         // https://docs.rs/optionable/latest/optionable/derive.Optionable.html
-        attr = "optionable(kube(resource), derive(Clone, Debug, Default, serde::Deserialize, serde::Serialize))",
+        attr = "optionable(kube(resource), derive(Clone, Debug, Default, Deserialize, Serialize))",
     )]
     pub struct CustomCrdSpec {
         pub msg: String,
         pub template: CustomCrdSpecTemplate,
     }
-);
+}
 
-with_common_derives!(
+with_common_derives! {
     pub struct CustomCrdSpecTemplate {
         // example for a rename use case as `type` is a reserved keyword in rust
         #[serde(rename = "type")]
         pub type_: CustomCrdSpecTemplateType,
         pub body: String,
     }
-);
+}
 
-with_common_derives!(
+with_common_derives! {
     pub enum CustomCrdSpecTemplateType {
         V1alpha1,
         V1beta1,
@@ -60,4 +55,4 @@ with_common_derives!(
         #[optionable_attr(default)]
         V1,
     }
-);
+}
