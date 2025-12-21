aebd69fae9478d72e5b3d180219280777e331f43
ngergs <niklas@ngergs.de>
2025-12-21 18:15:16 +0100

    chore: clippy

--- optionable/src/jiff02.rs
+++ optionable/src/jiff02.rs
@@ -1,4 +1,4 @@
 use crate::optionable::impl_optional_self;
-use jiff02::{SignedDuration, Span, SpanFieldwise, SpanRelativeTo, Timestamp, Zoned};
+use jiff02::{SignedDuration, Span, SpanFieldwise, Timestamp, Zoned};
 
 impl_optional_self!(SignedDuration, Span, SpanFieldwise, Timestamp, Zoned);
