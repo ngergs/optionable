0615ed5c65078c7f789acf40c7f1077cac196297
ngergs <niklas@ngergs.de>
2026-01-14 21:49:23 +0100

    chore: clippy and docs.rs adjustment

--- testscripts/other.sh
+++ testscripts/other.sh
@@ -9,7 +9,7 @@ fi
 cd ..
 
 cargo audit
-cargo clippy --features std,derive,chrono04,jiff02,serde_json,k8s_openapi027_v1_35,kube3,kube3/derive
+cargo clippy --workspace --exclude optionable_k8s_example --features std,derive,chrono04,jiff02,serde_json,k8s_openapi027_v1_35,kube3,kube3/derive
 cargo +nightly docs-rs -p optionable
 cargo msrv verify --path optionable_codegen
 cargo msrv verify --path optionable_derive
