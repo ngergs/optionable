55f9d772799f027fc75e894a2ce11f1487d2cf3b
ngergs <niklas@ngergs.de>
2026-04-26 22:14:16 +0200

    chore: regenerate k8s-openapi (fixed merge/list types)

--- optionable/src/k8s_openapi027/v1_35/api/core/v1/iscsi_persistent_volume_source.rs
+++ optionable/src/k8s_openapi027/v1_35/api/core/v1/iscsi_persistent_volume_source.rs
@@ -177,7 +177,7 @@ for k8s_openapi027::api::core::v1::ISCSIPersistentVolumeSource {
         } else if let Some(self_value) = self.secret_ref.as_mut()
             && let Some(other_value) = other.secret_ref
         {
-            crate::OptionableConvert::merge(self_value, other_value)?;
+            *self_value = crate::OptionableConvert::try_from_optioned(other_value)?;
         }
         if let Some(other_value) = other.target_portal {
             self.target_portal = crate::OptionableConvert::try_from_optioned(
